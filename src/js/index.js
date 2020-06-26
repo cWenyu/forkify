2a4690a059540fdaeeb6165227a60372a1153306
Wenyu Shi <d00198805@student.dkit.ie>
2020-06-26 16:31:49 +0100

    basic function set up

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,41 @@
-// Global app controller
-import num from './test'
+import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements } from './views/base';
+/**
+ * Global state of the app
+ * 1. search object
+ * 2. current recipe object
+ * 3. shopping list object
+ * 4. linked recipe
+ * 
+ */
+const state = {};
 
-const x = 23;
-console.log(`import ${num} from another module from test.js variables x: ${x}`);
+const controlSearch = async () => {
+    //1. get query from view
+    const query = searchView.getInput();
+
+    if (query) {
+        //2. create a new object
+        state.search = new Search(query);
+
+        //3. prepare UI for results
+        searchView.clearInput();
+        searchView.clearResults();
+
+        //4. search for recipes
+        await state.search.getResults();
+
+        //5. render results on UI
+        searchView.renderResults(state.search.result)
+
+    }
+
+}
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
+
+
+// const res = await axios(`https://forkify-api.herokuapp.com/api/get?rId=${this.id}`);
