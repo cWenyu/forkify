2e8dfbd753914ef04c6fd79a680a995b2a9a8b4d
Wenyu Shi <d00198805@student.dkit.ie>
2020-07-01 17:47:13 +0100

    fix recipeView formateCount

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -7,18 +7,21 @@ export const clearRecipe = () => {
 
 const formateCount = count => {
     if (count) {
-        // count = 2.5
-        const [int, dec] = count.toString().split('.').map(el => parseInt(el));
-        if (!dec) return count;
+        // count = 2.5 --> 5/2 --> 2 1/2
+        // count = 0.5 --> 1/2
+        const newCount = Math.round(count * 10000) / 10000;
+        const [int, dec] = newCount.toString().split('.').map(el => parseInt(el, 10));
+
+        if (!dec) return newCount;
+
         if (int === 0) {
-            const fr = new Fraction(count);
+            const fr = new Fraction(newCount);
             return `${fr.numerator}/${fr.denominator}`;
         } else {
-            const fr = new Fraction(count - int);
+            const fr = new Fraction(newCount - int);
             return `${int} ${fr.numerator}/${fr.denominator}`;
         }
     }
-    return '?';
 };
 
 const createIngredient = ingredient => `
