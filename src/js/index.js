c2f91b7815662ae3eb1a802ca9819892f90c13a1
Wenyu Shi <d00198805@student.dkit.ie>
2020-07-02 14:21:49 +0100

    finish project

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,12 @@
 import Search from './models/Search';
+import Recipe from './models/recipes';
+import List from './models/List';
+import Likes from './models/Likes';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
+import * as listView from './views/listView';
+import * as likesView from './views/likesView';
 import { elements, renderLoader, clearLoader } from './views/base';
-import Recipe from './models/recipes';
 /**
  * controller file
  * Global state of the app
@@ -58,7 +62,6 @@ elements.searchResPages.addEventListener('click', e => {
 const controlRecipe = async () => {
     //get Id from url 
     const id = window.location.hash.replace('#', '');
-    console.log(id);
     if (id) {
         //prepare UI for changes
         recipeView.clearRecipe();
@@ -76,7 +79,6 @@ const controlRecipe = async () => {
             //get recipe data and parse ingredients
             await state.recipe.getRecipe();
             state.recipe.parseIngredients();
-            console.log(state.recipe);
 
             //calculate servings and time
             state.recipe.calcTime();
@@ -84,7 +86,7 @@ const controlRecipe = async () => {
 
             //render recipes
             clearLoader();
-            recipeView.renderRecipe(state.recipe);
+            recipeView.renderRecipe(state.recipe, state.likes.isLiked(id));
         } catch (error) {
             alert('Error processing recipe')
         }
@@ -93,3 +95,96 @@ const controlRecipe = async () => {
 // window.addEventListener('hashchange', controlRecipe);
 // window.addEventListener('load', controlRecipe);
 ['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
+
+// List controller
+const controlList = () => {
+    // create a nw list if there is none yet
+    if (!state.list) state.list = new List();
+    //add each ingredient to the list 
+    state.recipe.ingredients.forEach(el => {
+        const item = state.list.addItem(el.count, el.unit, el.ingredient);
+        listView.renderItem(item);
+    })
+};
+
+// handle delete/update list item events
+elements.shopping.addEventListener('click', e => {
+    const id = e.target.closest('.shopping__item').dataset.itemid;
+
+    //handle delete button
+    if (e.target.matches('.shopping__delete, .shopping__delete *')) {
+        //delete from state
+        state.list.deleteItem(id);
+
+        //delete from UI
+        listView.deleteItem(id);
+    } else if (e.target.matches('.shopping__count-value')) {
+        //read data from UI
+        const val = parseFloat(e.target.value, 10);
+        state.list.updateItem(id, val);
+    }
+});
+
+// Likes controller
+const controlLike = () => {
+    if (!state.likes) state.likes = new Likes();
+    const currentID = state.recipe.id;
+
+    if (!state.likes.isLiked(currentID)) {
+        //User has not yet liked current recipe
+        const newLike = state.likes.addLike(
+            currentID,
+            state.recipe.title,
+            state.recipe.author,
+            state.recipe.img,
+        )
+        //toggle the like button
+        likesView.toggleLikeBtn(true);
+        //add like to UI like
+        likesView.renderLike(newLike);
+    } else {
+        //User has liked current recipe
+        //Remove like from the state
+        state.likes.deleteLike(currentID);
+        //toggle the like button
+        likesView.toggleLikeBtn(false);
+        //delete like from UI like
+        likesView.deleteLike(currentID);
+    }
+    likesView.toggleLikeMenu(state.likes.getNumLike());
+};
+
+//restore liked recipes on page load
+window.addEventListener('load', () => {
+    state.likes = new Likes();
+    //restore likes
+    state.likes.readStorage();
+    //toggle like menu
+    likesView.toggleLikeMenu(state.likes.getNumLike());
+    //render the existing lieks
+    state.likes.likes.forEach(like => likesView.renderLike(like));
+
+});
+
+//handling recipe button clicks
+elements.recipe.addEventListener('click', e => {
+    // .button-decrease * means any child of button-decrease
+    if (e.target.matches('.button-decrease, .button-decrease *')) {
+        //decrease button clicked
+        if (state.recipe.servings > 1) {
+            state.recipe.updateServings('dec');
+            recipeView.updateServingsIngredients(state.recipe);
+        }
+    } else if (e.target.matches('.button-increase, .button-increase *')) {
+        //increase button clicked
+        state.recipe.updateServings('inc');
+        recipeView.updateServingsIngredients(state.recipe);
+    } else if (e.target.matches('.recipe__btn--add, .recipe__btn--add *')) {
+        // add ingredients to shopping list
+        controlList();
+    } else if (e.target.matches('.recipe__love, .recipe__love *')) {
+        // add 
+        controlLike();
+    }
+});
+
--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -37,7 +37,7 @@ const createIngredient = ingredient => `
     </li>   
 `;
 
-export const renderRecipe = recipe => {
+export const renderRecipe = (recipe, isLiked) => {
     const markup = `
     <figure class="recipe__fig">
         <img src="${recipe.img}" alt="${recipe.title}" class="recipe__img">
@@ -61,12 +61,12 @@ export const renderRecipe = recipe => {
             <span class="recipe__info-text"> servings</span>
 
             <div class="recipe__info-buttons">
-                <button class="btn-tiny">
+                <button class="btn-tiny button-decrease">
                     <svg>
                         <use href="img/icons.svg#icon-circle-with-minus"></use>
                     </svg>
                 </button>
-                <button class="btn-tiny">
+                <button class="btn-tiny button-increase">
                     <svg>
                         <use href="img/icons.svg#icon-circle-with-plus"></use>
                     </svg>
@@ -76,7 +76,7 @@ export const renderRecipe = recipe => {
         </div>
         <button class="recipe__love">
             <svg class="header__likes">
-                <use href="img/icons.svg#icon-heart-outlined"></use>
+                <use href="img/icons.svg#icon-heart${isLiked ? '' : '-outlined'}"></use>
             </svg>
         </button>
     </div>
@@ -88,7 +88,7 @@ export const renderRecipe = recipe => {
         ${recipe.ingredients.map(el => createIngredient(el)).join('')} 
         </ul>
 
-        <button class="btn-small recipe__btn">
+        <button class="btn-small recipe__btn recipe__btn--add">
             <svg class="search__icon">
                 <use href="img/icons.svg#icon-shopping-cart"></use>
             </svg>
@@ -112,4 +112,14 @@ export const renderRecipe = recipe => {
     </div>
     `;
     elements.recipe.insertAdjacentHTML('afterbegin', markup);
+};
+
+export const updateServingsIngredients = recipe => {
+    //update servings
+    document.querySelector('.recipe__info-data--people').textContent = recipe.servings;
+    //update ingredients
+    const countElement = Array.from(document.querySelectorAll('.recipe__count'));
+    countElement.forEach((el, i) => {
+        el.textContent = formateCount(recipe.ingredients[i].count);
+    })
 };
